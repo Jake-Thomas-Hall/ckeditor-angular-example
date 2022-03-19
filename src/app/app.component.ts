45a83522bba8b31d5efeca6b5ab5f16598edca29
Diedinium <jakethomashall@gmail.com>
2022-03-19 15:11:44 +0000

    Implement Ckeditor example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ckeditor-testing';
+export class AppComponent implements AfterViewInit {
+  editor!: ClassicEditor;
+  savedContent: string = '';
+  @ViewChild('editor') editorElement!: ElementRef;
+
+  ngAfterViewInit(): void {
+    ClassicEditor.create(this.editorElement.nativeElement).then((editor: ClassicEditor) => {
+      this.editor = editor;
+      this.editor.model.document.on('change:data', () => this.saveContent());
+    });
+  }
+
+  saveContent(): void {
+    this.savedContent = this.editor.getData();
+  }
+
+  setContent(): void {
+    this.editor.setData('<p>Imagine you pulled this string from your database :)</p>');
+  }
 }
